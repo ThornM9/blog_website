f89e1e4a020ac2e0c50bb00e107ea0984ce09a1a
Thorn <tm.jalal@gmail.com>
2018-05-16 08:57:56 -0400

    user signup from UI including error and success messages

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-root 'pages#home'
-get 'pages/about', to: 'pages#about' 
+  root 'pages#home'
+  get 'pages/about', to: 'pages#about' 
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
