5e629b5934570fdece70669f8ae60e11c134434f
Thorn <tm.jalal@gmail.com>
2018-05-18 00:08:22 -0400

    complete user authentications

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   get 'pages/about', to: 'pages#about' 
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
